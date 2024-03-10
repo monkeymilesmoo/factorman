ea36175fa6846abd45cd8cdcf8b824eb8786b8a0
Monkeymilesmoo <miles@secretagents.com>
2024-03-10 13:12:27 -0400

    Add functionality to make and save/load chunks. Not used for actual map yet

--- src/Chunk/Chunk.java
+++ src/Chunk/Chunk.java
@@ -1,5 +1,20 @@
 package src.Chunk;
 
-public class Chunk {
+import java.io.Serializable;
+
+public class Chunk implements Serializable{
+	private int[][] tiles;
+
+	public Chunk() {
+		tiles = new int[32][32];
+	}
+
+	public void setTile(int x, int y, int tileID) {
+
+		tiles[x][y] = tileID;
+	}
 	
+	public int getTile(int x, int y){
+		return tiles[x][y];
+	}
 }
