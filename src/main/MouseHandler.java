57e9ddcb5bcfc1b963f0f6f8dae8dabb6400be82
Monkeymilesmoo <miles@secretagents.com>
2024-03-17 23:35:12 -0400

    selector now works kinda

--- src/main/MouseHandler.java
+++ src/main/MouseHandler.java
@@ -1,22 +1,34 @@
 package src.main;
 
+import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 import src.Chunk.Chunk;
+import src.tileEntity.TileEntity;
 
 
 public class MouseHandler extends MouseAdapter{
 
 	public boolean leftMouseClicked, rightMouseClicked;
 	public int mouseX, mouseY;
+	public int mouseWorldX, mouseWorldY;
 	public int mouseCol, mouseRow;
 	public int mouseTileX, mouseTileY;
+
+	BufferedImage selectionYellow;
 
 	GamePanel gp;
 
 	public MouseHandler(GamePanel gp){
 		this.gp = gp;
+		try{
+			selectionYellow = ImageIO.read(getClass().getResourceAsStream("/res/core/cursor-boxes-32x32.png")).getSubimage(0, 0, 64, 64);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	public void mousePressed(MouseEvent e){
@@ -56,12 +68,34 @@ public class MouseHandler extends MouseAdapter{
 		mouseX = e.getX();
 		mouseY = e.getY();
 
-		mouseX = mouseX + gp.player.worldX - gp.player.screenX;
-		mouseY = mouseY + gp.player.worldY - gp.player.screenY;
-		mouseTileX = (mouseX % (Chunk.chunkSize * GamePanel.tileSize)) / GamePanel.tileSize;
-		mouseTileY = (mouseY % (Chunk.chunkSize * GamePanel.tileSize)) / GamePanel.tileSize;
-		mouseCol = (mouseX - mouseTileX) / (Chunk.chunkSize * GamePanel.tileSize);
-		mouseRow = (mouseY - mouseTileY) / (Chunk.chunkSize * GamePanel.tileSize);
+		mouseWorldX = mouseX + gp.player.worldX - gp.player.screenX;
+		mouseWorldY = mouseY + gp.player.worldY - gp.player.screenY;
+		mouseTileX = (mouseWorldX % (Chunk.chunkSize * GamePanel.tileSize)) / GamePanel.tileSize;
+		mouseTileY = (mouseWorldY % (Chunk.chunkSize * GamePanel.tileSize)) / GamePanel.tileSize;
+		mouseCol = (mouseWorldX - mouseTileX) / (Chunk.chunkSize * GamePanel.tileSize);
+		mouseRow = (mouseWorldY - mouseTileY) / (Chunk.chunkSize * GamePanel.tileSize);
+		System.out.println("" + (gp.chunkGrid.getChunk(mouseCol, mouseRow).getTileEntity(mouseTileX, mouseTileY)));
+
+	}
+
+	public void hoveredTileEntity(Graphics2D g2){
+
+	
+		try{ //TODO FIX THIS maybe set chunk as a variable first and check if null
+			TileEntity hovered = gp.chunkGrid.getChunk(mouseCol, mouseRow).getTileEntity(mouseTileX, mouseTileY);
+
+			if (hovered != null){
+
+				int hoveredWorldX = (mouseCol * Chunk.chunkSize * GamePanel.tileSize) + (mouseTileX * GamePanel.tileSize) - gp.player.worldX + gp.player.screenX;
+				int hoveredWorldY = (mouseRow * Chunk.chunkSize * GamePanel.tileSize) + (mouseTileY * GamePanel.tileSize) - gp.player.worldY + gp.player.screenY;
+				
+				g2.drawImage(selectionYellow, hoveredWorldX, hoveredWorldY, GamePanel.tileSize * hovered.tileWidth, GamePanel.tileSize* hovered.tileHeight, null);
+
+			}
+		}catch(Exception e){
+
+		}
+
 	}
 	
 }
--- src/tileEntity/tileEntity.java
+++ src/tileEntity/tileEntity.java
@@ -5,6 +5,22 @@ import java.io.Serializable;
 
 public class TileEntity implements Serializable{
 	//ALL BUILDING AND NON BUILDING VARIABLES MUST BE DECLARED HERE
-	short textureID;
-	byte tileWidth, tileHeight;
+	public short textureID;
+	public byte tileWidth, tileHeight;
+	public byte xOffset, yOffset;
+
+	public TileEntity(byte tileWidth, byte tileHeight, short tileEntityID){
+
+		this.tileHeight = tileHeight;
+		this.tileWidth = tileWidth;
+
+		
+
+
+	}
+
+	public void beingMined(){
+
+	}
+
 }
