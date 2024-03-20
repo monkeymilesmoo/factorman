34dbc12e64d3c04c25f2ddbe88d403ce9f79dfbf
Monkeymilesmoo <miles@secretagents.com>
2024-03-19 20:38:10 -0400

    Make buildingimages a hashmap not an array. not sure if this is slower or not

--- src/tileEntity/Building.java
+++ src/tileEntity/Building.java
@@ -1,10 +1,14 @@
 package src.tileEntity;
 
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
 
 public class Building extends TileEntity{
 
 
-	public transient static EntityImage[] buildingImages;
+	// public transient static EntityImage[] buildingImages;
+	public transient static HashMap<String, EntityImage> buildingImages = new HashMap<String, EntityImage>();
+
 	// public transient static HashMap<String, EntityImage> buildingImages = new HashMap<String, EntityImage>();
 
 	byte rotation = 2;
@@ -18,7 +22,7 @@ public class Building extends TileEntity{
 
 	public static void loadBuildingImages(){
 
-		buildingImages = new EntityImage[3];
+		// buildingImages = new EntityImage[3];
 
 
 
@@ -27,10 +31,9 @@ public class Building extends TileEntity{
 			
 			
 			// buildingImages.put("assembling-machine-1", new EntityImage("building/assembling-machine-1/assembling-machine-1", 32, 1));
-			buildingImages[0] = new EntityImage("building/assembling-machine-1/assembling-machine-1", 32, 1, 3, 3);
-			buildingImages[1] = new EntityImage("building/assembling-machine-1/assembling-machine-1-shadow", 32, 1, 3, 3);
-			buildingImages[2] = new EntityImage("building/steel-chest/steel-chest", 1, 1, 1, 1);
-
+			buildingImages.put("assembling-machine-1", new EntityImage("building/assembling-machine-1/assembling-machine-1", 32, 1, 3, 3));
+			buildingImages.put("assembling-machine-1-shadow", new EntityImage("building/assembling-machine-1/assembling-machine-1-shadow", 32, 1, 3, 3));
+			buildingImages.put("steel-chest", new EntityImage("building/steel-chest/steel-chest", 1, 1, 1, 1));
 
 			// buildingImages[0] = ImageIO.read(getClass().getResourceAsStream("/res/tileEntity/assembling-machine-1/assembling-machine-1.png"));
 			// buildingImages[1] = ImageIO.read(getClass().getResourceAsStream("/res/tileEntity/steel-chest/steel-chest.png"));
