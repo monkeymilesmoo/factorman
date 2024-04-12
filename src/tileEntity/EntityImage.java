9b54799f96bce565ed2c62078bc8300d3e0cc7cf
Monkeymilesmoo <miles@secretagents.com>
2024-04-11 23:52:58 -0400

    all the afformentioned itemproperties now have corresponding images

--- src/tileEntity/EntityImage.java
+++ src/tileEntity/EntityImage.java
@@ -166,82 +166,89 @@ public class EntityImage {
 
 
 			//TODO for now, just the icons are being used, but eventually fill these out for each building
+
+			//Containers
 			putImage("wooden-chest", true, 1, 1, 0, 0, 0, 0, 0, 0);
 			putImage("iron-chest", true, 1, 1, 0, 0, 0, 0, 0, 0);
 			putImage("steel-chest", true, 1, 1, 50, 0, 1, 1, 0, 10);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
-			putImage("", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("storage-tank", true, 1, 1, 0, 0, 0, 0, 0, 0);
+
+			//Belts
+			putImage("transport-belt", true, 1, 1, 0, 0, 0, 0, 0, 0); 
+			putImage("fast-transport-belt", true, 1, 1, 0, 0, 0, 0, 0, 0); 
+			putImage("express-transport-belt", true, 1, 1, 0, 0, 0, 0, 0, 0); 
+			//Underground belts
+			putImage("underground-belt", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("fast-underground-belt", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("express-underground-belt", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			//Splitters
+			putImage("splitter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("fast-splitter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("express-splitter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+
+			//Inserters
+			putImage("burner-inserter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("inserter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("long-handed-inserter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("fast-inserter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("filter-inserter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("stack-inserter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("stack-filter-inserter", true, 1, 1, 0, 0, 0, 0, 0, 0);
+
+			//Power poles
+			putImage("small-electric-pole", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("medium-electric-pole", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("big-electric-pole", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("substation", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			//Pipes
+			putImage("pipe", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("pipe-to-ground", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("pump", true, 1, 1, 0, 0, 0, 0, 0, 0);
+
+			//Train items
+			putImage("rail", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("train-stop", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("rail-signal", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("rail-chain-signal", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("locomotive", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("cargo-wagon", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("fluid-wagon", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("artillery-wagon", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			
+			//Transport
+			putImage("car", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("tank", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("spidertron", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("spidertron-remote", true, 1, 1, 0, 0, 0, 0, 0, 0);
+
+			//Logistics
+			putImage("logistic-robot", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("construction-robot", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("logistic-chest-active-provider", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("logistic-chest-passive-provider", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("logistic-chest-storage", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("logistic-chest-buffer", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("logistic-chest-requester", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("robotport", true, 1, 1, 0, 0, 0, 0, 0, 0);
+
+			//Circuit network
+			putImage("small-lamp", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("red-wire", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("green-wire", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("arithmetic-combinator", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("decider-combinator", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("constant-combinator", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("power-switch", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("programmable-speaker", true, 1, 1, 0, 0, 0, 0, 0, 0);
+
+			//Terrain
+			putImage("stone-brick", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("concrete", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("hazard-concrete", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("refined-concrete", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("refined-hazard-concrete", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("landfill", true, 1, 1, 0, 0, 0, 0, 0, 0);
+			putImage("cliff-explosives", true, 1, 1, 0, 0, 0, 0, 0, 0);
 
 
 
